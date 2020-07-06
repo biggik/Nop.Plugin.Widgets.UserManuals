d7a5afdab4293c25ca2ea90f87c4291664e8e1ce
Birgir Kristmannsson <birgir@centara.com>
2020-07-06 23:13:02 +0000

    Added submission files

--- build-all.cmd
+++ build-all.cmd
@@ -1,15 +1,16 @@
 @echo off
 set project=Nop.Plugin.Widgets.UserManuals
 
-REM cd 4.30\%project%
-REM Echo building 4.30 of %project%
-REM rd ..\_build\release\. /s /q > 0
-REM dotnet build %project%.csproj --configuration=Release --no-incremental
-rem cd ..\..
+cd 4.30\%project%
+Echo building 4.30 of %project%
+rd ..\_build\release\. /s /q > 0
+dotnet build %project%.csproj --configuration=Release --no-incremental
+
+cd ..\..
 
 cd 4.20\%project%
 Echo building 4.20 of %project%
 rd ..\_build\release\. /s /q > 0
 dotnet build %project%.csproj --configuration=Release --no-incremental
 
-cd ..
+cd ..\..

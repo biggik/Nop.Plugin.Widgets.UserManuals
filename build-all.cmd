9a56d51165bd18b5989cdc1618bdc0ed615c6d37
Birgir Kristmannsson <bk@mf.is>
2021-08-17 00:36:39 +0000

    Updated nopPackage

--- build-all.cmd
+++ build-all.cmd
@@ -3,21 +3,21 @@ set project=Nop.Plugin.Widgets.UserManuals
 
 cd 4.40\%project%
 Echo building 4.40 of %project%
-rd ..\_build\release\. /s /q > 0
+rd ..\_build\release\. /s /q > nul
 dotnet build %project%.csproj --configuration=Release --no-incremental
 
 cd ..\..
 
 cd 4.30\%project%
 Echo building 4.30 of %project%
-rd ..\_build\release\. /s /q > 0
+rd ..\_build\release\. /s /q > nul
 dotnet build %project%.csproj --configuration=Release --no-incremental
 
 cd ..\..
 
 cd 4.20\%project%
 Echo building 4.20 of %project%
-rd ..\_build\release\. /s /q > 0
+rd ..\_build\release\. /s /q > nul
 dotnet build %project%.csproj --configuration=Release --no-incremental
 
 cd ..\..

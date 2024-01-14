6430bca33b799ba51e86c0d7f931966ac1704248
Birgir Kristmannsson <bk@mf.is>
2024-01-14 23:29:58 +0000

    Updated

--- Common/Data/SchemaMigration.cs
+++ Common/Data/SchemaMigration.cs
@@ -1,6 +1,8 @@
 ﻿using FluentMigrator;
 using Nop.Data.Migrations;
 using Nop.Plugin.Widgets.UserManuals.Domain;
+using FluentMigrator.Infrastructure;
+
 #if NOP_45
 using Nop.Data.Extensions;
 #endif
@@ -10,7 +12,7 @@ namespace Nop.Plugin.Widgets.UserManuals.Data
 #if !NOP_45
     [SkipMigrationOnUpdate]
 #endif
-    [NopMigration("2020/07/04 09:10:11:6455422", "UserManuals Plugin base schema")]
+    [NopMigration("2020/07/04 09:10:11:6455422", "UserManuals Plugin base schema", MigrationProcessType.Installation)]
     public class SchemaMigration : AutoReversingMigration
     {
 #if !NOP_45
@@ -25,14 +27,39 @@ namespace Nop.Plugin.Widgets.UserManuals.Data
         public override void Up()
         {
 #if NOP_45
-            Create.TableFor<UserManualProduct>();
-            Create.TableFor<UserManualCategory>();
-            Create.TableFor<UserManual>();
+            if (!_productExists)
+            {
+                Create.TableFor<UserManualProduct>();
+            }
+            if (!_categoryExists)
+            {
+                Create.TableFor<UserManualCategory>();
+            }
+            if (!_manualExists)
+            {
+                Create.TableFor<UserManual>();
+            }
 #else
             _migrationManager.BuildTable<UserManualProduct>(Create);
             _migrationManager.BuildTable<UserManualCategory>(Create);
             _migrationManager.BuildTable<UserManual>(Create);
 #endif
         }
+
+#if NOP_45
+        private bool _productExists;
+        private bool _categoryExists;
+        private bool _manualExists;
+        public override void GetUpExpressions(IMigrationContext context)
+        {
+            // Schema migration is always attempting to create the tables, so override 
+            // here to get out of this situation
+            _productExists = context.QuerySchema.TableExists("dbo", "StatusUserManualProduct");
+            _categoryExists = context.QuerySchema.TableExists("dbo", "StatusUserManualCategory");
+            _manualExists = context.QuerySchema.TableExists("dbo", "StatusUserManual");
+
+            base.GetUpExpressions(context);
+        }
+#endif
     }
 }
